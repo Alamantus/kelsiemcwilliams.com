f8ea7fbfed6022cf35724f703df69b45e85a20a7
Robbie Antenesse <dev@alamantus.com>
2017-06-13 23:51:35 -0600

    Finished CV, Resume, and  Writing Samples pages

--- js/control.js
+++ js/control.js
@@ -1,13 +1,32 @@
+var sitePages = [
+  '#about',
+  '#portfolio',
+  '#cv',
+  '#resume',
+];
+var cvPages = [
+  '#cv-education',
+  '#cv-employment',
+  '#cv-teaching',
+  '#cv-publications',
+  '#cv-administration',
+  '#cv-trainings',
+  '#cv-development',
+  '#cv-presentations',
+  '#cv-volunteer',
+];
+
 $(document).ready(function () {
   // Hide inactive pages
-  $('#portfolio, #cv, #resume, #cv-employment, #cv-teaching, #cv-publications').hide();
+  $(sitePages.slice(1).join(', ')).hide();
+  $(cvPages.slice(1).join(', ')).hide();
 
-  $('#about-button, #portfolio-button, #cv-button, #resume-button').click(function () {
+  $(sitePages.map(mapPageButtons).join(', ')).click(function () {
     var pageName = $(this).attr('id').replace('-button', '');
     togglePage(pageName);
   });
 
-  $('#cv-education-tab, #cv-employment-tab, #cv-teaching-tab, #cv-publications-tab').click(function () {
+  $(cvPages.map(mapCVTabs).join(', ')).click(function () {
     var tabName = $(this).attr('id').replace('-tab', '');
     toggleCVSection(tabName);
   });
@@ -23,16 +42,40 @@ $(document).ready(function () {
   });
 });
 
+function mapPageButtons (item) {
+  return item + '-button';
+}
+
+function mapCVTabs (item) {
+  return item + '-tab';
+}
+
 function togglePage (activatePage) {
-  $('#about, #portfolio, #cv, #resume').hide(400);
-  $('#about-button, #portfolio-button, #cv-button, #resume-button').removeClass('active');
+  var sitePagesExcludingActivated = sitePages.slice();
+  var sectionIndex = sitePagesExcludingActivated.indexOf('#' + activatePage);
+
+  if (sectionIndex >= 0) {
+    sitePagesExcludingActivated.splice(sectionIndex, 1);
+  }
+
+  $(sitePagesExcludingActivated.join(', ')).hide(400);
+  $(sitePagesExcludingActivated.map(mapPageButtons).join(', ')).removeClass('active');
+
   $('#' + activatePage).show(400);
   $('#' + activatePage + '-button').addClass('active');
 }
 
 function toggleCVSection (activateSection) {
-  $('#cv-education, #cv-employment, #cv-teaching, #cv-publications').hide(400);
-  $('#cv-education-tab, #cv-employment-tab, #cv-teaching-tab, #cv-publications-tab').removeClass('active');
+  var cvPagesExcludingActivated = cvPages.slice();
+  var sectionIndex = cvPagesExcludingActivated.indexOf('#' + activateSection);
+
+  if (sectionIndex >= 0) {
+    cvPagesExcludingActivated.splice(sectionIndex, 1);
+  }
+
+  $(cvPagesExcludingActivated.join(', ')).hide(400);
+  $(cvPagesExcludingActivated.map(mapCVTabs).join(', ')).removeClass('active');
+
   $('#' + activateSection).show(400);
   $('#' + activateSection + '-tab').addClass('active');
 }
